372a47b7c64e0cb569a7619348065532d79c4550
CristianFortich <yezidfortich@gmail.com>
2022-08-23 14:23:02 -0500

    sync models with back

--- src/database/database.js
+++
@@ -1,3 +0,0 @@
-import Sequelize from "sequelize";
-
-export const sequelize = new Sequelize('postgres://postgres:yezidfortich@localhost:5433/viveargentina')
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import app from './app.js'
-import {sequelize} from './database/database.js'
+// import app from './app.js'
+// import {conn} from './database/database.js'
 
-async function main(){
-    try {
-        await sequelize.authenticate()
-        console.log('conection has been stablished successfully')
-        app.listen(4000)
-        console.log('server is listening on prot ', 4000)
-    } catch (error) {
-        console.log('unable to conect to the database: ', error)
-    }
-}
+const app = require('./app.js');
+const { conn } = require('./database.js');
 
-main();
+// Syncing all the models at once.
+// force/alter
+conn.sync({ alter: true }).then(() => {
+  app.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
+// async function main(){
+//     try {
+//         await conn.authenticate()
+//         console.log('conection has been stablished successfully')
+//         app.listen(4000)
+//         console.log('server is listening on prot ', 4000)
+//     } catch (error) {
+//         console.log('unable to conect to the database: ', error)
+//     }
+// }
+
+// main();
