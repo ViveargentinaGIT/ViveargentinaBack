ea2934b4caa22cc702a52e25c3dfc0451de210a8
clanrosso <claudiorossocba@hotmail.com.ar>
2022-08-26 00:16:03 -0300

    Copio ultima version de  database que hizo Cristian para el deploy

--- src/database.js
+++ src/database.js
@@ -5,7 +5,16 @@ const fs = require("fs");
 const path = require("path");
 
 const sequelize = new Sequelize(
-  "postgres://glzzauxhpjqfru:260e621a005c4f37e4efd22637ebf8844684e548cd7312f706556520709e5a0d@ec2-34-227-135-211.compute-1.amazonaws.com:5432/d818agm2f2a734"
+  "postgres://glzzauxhpjqfru:260e621a005c4f37e4efd22637ebf8844684e548cd7312f706556520709e5a0d@ec2-34-227-135-211.compute-1.amazonaws.com:5432/d818agm2f2a734",
+  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  }
 );
 
 const basename = path.basename(__filename);
