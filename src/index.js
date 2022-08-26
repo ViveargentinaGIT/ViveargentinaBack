d188c95a5f23a8353418dca728b07ea716332883
CristianFortich <yezidfortich@gmail.com>
2022-08-25 20:24:17 -0500

    fixes static PORT

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const { conn } = require("./database.js");
 
 // Syncing all the models at once.
 // force/alter
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 conn.sync({ alter: true }).then(() => {
   app.listen(port, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("%s listening at ", port); // eslint-disable-line no-console
   });
 });
 
