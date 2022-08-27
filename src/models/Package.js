4452a566097194fc851302d31247f896719b25f6
clanrosso <claudiorossocba@hotmail.com.ar>
2022-08-27 20:30:36 -0300

    images  con datatype text(medium)

--- src/models/Package.js
+++ src/models/Package.js
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       image: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT("medium"),
         // allowNull: false
       },
       video: {
