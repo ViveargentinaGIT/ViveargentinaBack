8fad93c99ac1ffd57befad4290080db6a3faa059
clanrosso <claudiorossocba@hotmail.com.ar>
2022-09-02 15:23:25 -0300

    Agregamos ruta get/users/:userId

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,25 @@
 const { Router } = require("express");
-const { User, Query, Review } = require("../database");
+const { User, Query, Review, Experience, Package } = require("../database");
 
 const router = Router();
 
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const selectedUser = await User.findByPk(userId, {
+      include: [Query, Review, Experience, Package],
+    });
+    return res.status(200).send(selectedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
-    const allUsers = await User.findAll({ include: [Query, Review] });
+    const allUsers = await User.findAll({
+      include: [Query, Review, Experience],
+    });
     return res.status(200).send(allUsers);
   } catch (err) {
     res.status(400).json({ error: err.message });
