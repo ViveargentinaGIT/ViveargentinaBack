35ea1754ca3d427a2d02f4e60780197c00dd5b42
CristianFortich <yezidfortich@gmail.com>
2022-09-04 15:30:45 -0500

    added login and singin routes and a verification function

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,71 @@
 const { Router } = require("express");
 const { User, Query, Review, Experience, Package } = require("../database");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const router = Router();
+
+function authenticateToken(req, res, next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token == null) return res.sendStatus(401)
+
+  jwt.verify(token, "henryboom", (err, user)=>{
+    if(err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+router.post("/singin", async (req, res)=>{
+  const {first_name, last_name, email, password, birth_date, photo} = req.body;
+  if (!first_name){
+    return res.status(404).send("You must enter a name, email and password to create a new user");
+  }
+  try {
+    const user = await User.findAll({
+      where:{
+        email: email
+      }
+    })
+    if(user.length>0)return res.status(400).send('There is already an user registered with this mail')
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      email,
+      password: hashedPassword,
+      birth_date,
+      photo,
+    });
+    return res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).send('error')
+  }
+})
+
+router.post("/login", async (req, res)=>{
+  const {email, password}= req.body;
+  try {
+    const user = await User.findAll({
+      where:{
+        email: email
+      }
+    })
+    if(user.length === 0){
+      res.status(404).send('user not found')
+    }
+    if(await bcrypt.compare(password, user[0].password)){
+      const id = user[0].id
+			const accessToken = jwt.sign(id, "henryboom")
+      res.status(201).json({accessToken: accessToken, auth: true, user: user[0]})
+		} else{
+			res.send('not allowed')
+		}
+  } catch (error) {
+    res.status(500).send('something went wrong')
+  }
+})
 
 router.get("/administrators", async (req, res) => {
   try {
