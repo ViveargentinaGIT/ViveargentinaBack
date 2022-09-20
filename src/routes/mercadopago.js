26e8e2562007d585ae7775af1e37ae4f4090c9d2
clanrosso <claudiorossocba@hotmail.com.ar>
2022-09-20 18:45:15 -0300

    Mercadopago3

--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -34,7 +34,7 @@ router.post("/", async (req, res) => {
   let preference = {
     items: arrayPreference,
     back_urls: {
-      success: "http://localhost:3000/home",
+      success: "https://experienceviveargentina.vercel.app/home",
       failure: "http://localhost:3000/home",
       pending: "http://localhost:3000/home",
     },
